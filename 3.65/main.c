01b7b19b97dbd484d445b21a59c776899271ce78
SKGleba <30833773+SKGleba@users.noreply.github.com>
2018-04-28 19:59:38 -0500

    Update main.c

--- 3.65/main.c
+++ 3.65/main.c
@@ -11,29 +11,28 @@
 #include <psp2/kernel/processmgr.h>
 #include <vita2d.h>
 #include <taihen.h>
-
+#include <stuff.h>
 #include "TIDUtils.h"
-#include "stuff.h"
 
 int main() {
 	SceCtrlData pad, old_pad;
 	unsigned int keys_down;
 	old_pad.buttons = 0;
-	int col1 = 92;
-	int col2 = 226;
-	int col3 = 92;
+	int col1 = 255;
+	int col2 = 255;
+	int col3 = 255;
 	vita2d_pgf *font;
 	vita2d_init();
 	vita2d_set_clear_color(RGBA8(0x00, 0x00, 0x00, 0xFF));
 	font = vita2d_load_default_pgf();
 	memset(&pad, 0, sizeof(pad));
 	
-	const char Disclaimer[512] = "Welcome to MiniRex - Project \"DeepLoad\" Flasher!\nThis installer overwrites MBR with a modified version.\nI provide this tool \"as is\" without warranty of any kind.\n\nAll credits go to Team Molecule for enso exploit\n\nMod by SKGleba (twitter.com/skgleba)";
+	const char Disclaimer[512] = "Welcome to MiniRex - Project \"DeepLoad\" Flasher!\nThis installer overwrites MBR with a modified version.\nI provide this tool \"as is\" without warranty of any kind.\n\nAll credits go to Team Molecule for enso exploit\n\n\nMod by SKGleba (twitter.com/skgleba)";
 	const char PSTVNotContinue[256] = "This installer can NOT continue.\n\nThis version is made for fw 3.65 ONLY\n\nPress X to exit...";
 	const char MainMenuOpts[5][256] = {"Flash DEX Spoof", "Flash TOOL Spoof", "Flash ITU Spoof", "Revert changes", "Exit this app"};
-	const char SubMenuOpts[5][256] = {"Deeper Spoof", "Flash RAW ITU spoof", "Flash ux0:skg/mrex/custom/fat.bin", "UI colour", "Exit this app"};
-	const char Sub2MenuOpts[4][256] = {"DEX", "ITU", "TOOL", "Exit this app"};
-	const char ColMenuOpts[5][256] = {"Red", "Green", "Dark Blue", "Light Blue", "Back"};
+	const char SubMenuOpts[5][256] = {"Deeper Spoof", "Flash RAW spoof", "Flash ux0:skg/mrex/custom/fat.bin", "UI colour", "Exit this app"};
+	const char nobMenuOpts[4][256] = {"DEX", "ITU", "TOOL", "Back"};
+	const char ColMenuOpts[5][256] = {"Red", "White", "Green", "Blue", "Back"};
 	const char YesNo[2][4] = {"No", "Yes"};
 	
 	int config_exists = exists("ur0:tai/boot_config.txt");
@@ -49,8 +48,8 @@ int main() {
 		vita2d_start_drawing();
 		vita2d_clear_screen();
 		if (exists("ur0:temp/mrexred") == 1){col1 = 226; col2 = 92; col3 = 92;}
-		if (exists("ur0:temp/mrexsbl") == 1){col1 = 0; col2 = 0; col3 = 226;}
-		if (exists("ur0:temp/mrexdbl") == 1){col1 = 92; col2 = 92; col3 = 226;}
+		if (exists("ur0:temp/mrexsbl") == 1){col1 = 0; col2 = 0; col3 = 255;}
+		if (exists("ur0:temp/mrexdbl") == 1){col1 = 92; col2 = 226; col3 = 92;}
 		
 		switch(UIScreen) {
 			case 0: // Disclaimer Message
@@ -92,6 +91,8 @@ int main() {
 				if (slimbin == 29) filecopy("ux0:app/SKGM1NR3X/stuff/deepd", "ur0:temp/mrexmbr");
 				if (slimbin == 89) filecopy("ux0:app/SKGM1NR3X/stuff/deepi", "ur0:temp/mrexmbr");
 				if (slimbin == 79) filecopy("ux0:app/SKGM1NR3X/stuff/mbroi", "ur0:temp/mrexmbr");
+				if (slimbin == 15) filecopy("ux0:app/SKGM1NR3X/stuff/mbrod", "ur0:temp/mrexmbr");
+				if (slimbin == 7) filecopy("ux0:app/SKGM1NR3X/stuff/mbrot", "ur0:temp/mrexmbr");
 				if (slimbin == 99) filecopy("ux0:app/SKGM1NR3X/stuff/deept", "ur0:temp/mrexmbr");
 				if (slimbin == 69) filecopy("ux0:skg/mrex/custom/fat.bin", "ur0:temp/mrexmbr");
 				filecopy("ur0:tai/config.txt", "ur0:temp/mrexflag");
@@ -101,7 +102,8 @@ int main() {
 					if (exists("ur0:temp/mrexflag") == 0) sceKernelExitProcess(0);
 					vita2d_pgf_draw_textf(font, 35, 115, RGBA8(255,255,255,255), 1.5f, "Flashing...\nDo not turn off your Vita or exit the app!");
 				} else if (doStuff == 3) {
-						flash(1, 1, 0, "ur0:temp/mrexmbr", 1, "ur0:temp/mrexflag", NULL);
+
+flash(1, 1, 0, "ur0:temp/mrexmbr", 1, "ur0:temp/mrexflag", NULL);
 
 					vita2d_pgf_draw_textf(font, 35, 115, RGBA8(255,255,255,255), 1.5f, "Cleaning up...");
 				} else if (doStuff == 4) {
@@ -137,17 +139,30 @@ int main() {
 		vita2d_clear_screen();
 				if (selected < 0) selected = 0;
 				if (selected > 3) selected = 3;
-				for (int i = 0; i < 5; i++) {
+				for (int i = 0; i < 4; i++) {
 					if (i == selected) {
 						vita2d_draw_rectangle(70, (80*i) + 85, 820, 70, RGBA8(col1,col2,col3,255));
-						vita2d_pgf_draw_text(font, 85, (80*i) + 130, RGBA8(0,0,0,255), 1.5f, Sub2MenuOpts[i]);
-					} else vita2d_pgf_draw_text(font, 85, (80*i) + 130, RGBA8(255,255,255,255), 1.5f, Sub2MenuOpts[i]);
+						vita2d_pgf_draw_text(font, 85, (80*i) + 130, RGBA8(0,0,0,255), 1.5f, nobMenuOpts[i]);
+					} else vita2d_pgf_draw_text(font, 85, (80*i) + 130, RGBA8(255,255,255,255), 1.5f, nobMenuOpts[i]);
+				}
+				break;
+			case 37: 
+				vita2d_pgf_draw_text(font, 215, 45, RGBA8(255,255,255,255), 1.5f, "Flash raw spoof");
+	vita2d_set_clear_color(RGBA8(0x00, 0x00, 0x00, 0xFF));
+		vita2d_clear_screen();
+				if (selected < 0) selected = 0;
+				if (selected > 3) selected = 3;
+				for (int i = 0; i < 4; i++) {
+					if (i == selected) {
+						vita2d_draw_rectangle(70, (80*i) + 85, 820, 70, RGBA8(col1,col2,col3,255));
+						vita2d_pgf_draw_text(font, 85, (80*i) + 130, RGBA8(0,0,0,255), 1.5f, nobMenuOpts[i]);
+					} else vita2d_pgf_draw_text(font, 85, (80*i) + 130, RGBA8(255,255,255,255), 1.5f, nobMenuOpts[i]);
 				}
 				break;
 			case 8: 
 				vita2d_pgf_draw_text(font, 215, 45, RGBA8(255,255,255,255), 1.5f, "UI Colour");
 		if (exists("ur0:temp/mrexred") == 1) sceIoRemove("ur0:temp/mrexred");
-		if (exists("ur0:temp/mrexgrn") == 1) sceIoRemove("ur0:temp/mrexsbl");
+		if (exists("ur0:temp/mrexsbl") == 1) sceIoRemove("ur0:temp/mrexsbl");
 		if (exists("ur0:temp/mrexdbl") == 1) sceIoRemove("ur0:temp/mrexdbl");
 				if (selected < 0) selected = 0;
 				if (selected > 4) selected = 4;
@@ -185,7 +200,7 @@ break;
 					break;
 				case 7:
 					if (selected == 0) {UIScreen = 27;}
-					if (selected == 1) {slimbin = 79; UIScreen = 3;}
+					if (selected == 1) UIScreen = 37;
 					if (selected == 2) {slimbin = 69; UIScreen = 3;}
 					if (selected == 3) {UIScreen = 8;}
 					if (selected == 4) UIScreen = 1;
@@ -196,11 +211,17 @@ break;
 					if (selected == 2) {slimbin = 99; UIScreen = 3;}
 					if (selected == 3) UIScreen = 7;
 					break;
+				case 37:
+					if (selected == 0) {slimbin = 15; UIScreen = 3;}
+					if (selected == 1) {slimbin = 79; UIScreen = 3;}
+					if (selected == 2) {slimbin = 7; UIScreen = 3;}
+					if (selected == 3) UIScreen = 7;
+					break;
 				case 8:
 					if (selected == 0) {filecopy("ur0:tai/config.txt", "ur0:temp/mrexred"); col1 = 226; col2 = 92; col3 = 92; UIScreen = 7;}
-					if (selected == 3) {filecopy("ur0:tai/config.txt", "ur0:temp/mrexsbl"); col1 = 0; col2 = 0; col3 = 226; UIScreen = 7;}
-					if (selected == 2) {filecopy("ur0:tai/config.txt", "ur0:temp/mrexdbl"); col1 = 92; col2 = 92; col3 = 226; UIScreen = 7;}
-					if (selected == 1) {col1 = 92; col2 = 226; col3 = 92; UIScreen = 7;}
+					if (selected == 1) {col1 = 255; col2 = 255; col3 = 255; UIScreen = 7;}
+					if (selected == 2) {filecopy("ur0:tai/config.txt", "ur0:temp/mrexdbl"); col1 = 92; col2 = 226; col3 = 92; UIScreen = 7;}
+					if (selected == 3) {filecopy("ur0:tai/config.txt", "ur0:temp/mrexsbl"); col1 = 0; col2 = 0; col3 = 255; UIScreen = 7;}
 					if (selected == 4) UIScreen = 7;
 					break;
 			 }
@@ -217,6 +238,12 @@ break;
 				case 8:
 					selected -= 1;
 					break;
+				case 27:
+					selected -= 1;
+					break;
+				case 37:
+					selected -= 1;
+					break;
 			}
 		} if (keys_down & SCE_CTRL_DOWN) {
 			switch(UIScreen) {
@@ -227,6 +254,12 @@ break;
 					selected += 1;
 					break;
 				case 8:
+					selected += 1;
+					break;
+				case 27:
+					selected += 1;
+					break;
+				case 37:
 					selected += 1;
 					break;
 			}
